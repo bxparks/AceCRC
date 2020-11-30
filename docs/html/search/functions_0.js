606ce51ffe63788141c23f76a1a3f940b5a75c67
Brian Park <brian@xparks.net>
2020-11-29 21:12:04 -0800

    docs: Generate doxygen docs

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['crc_5freflect_28',['crc_reflect',['../crc32__bit_8cpp.html#a0a946337bae423c0f6734995a98999a7',1,'ace_crc::crc32_bit']]],
+  ['crc_5fupdate_29',['crc_update',['../crc16ccitt__bit_8cpp.html#a72f1a838b36e313fbebdf0b521a99a7b',1,'ace_crc::crc16ccitt_bit::crc_update()'],['../crc16ccitt__byte_8cpp.html#aa38a83b05e8244b0407fa92e349d359a',1,'ace_crc::crc16ccitt_byte::crc_update()'],['../crc16ccitt__nibble_8cpp.html#ad29748b4b0a22231a71d65ba1329b4d1',1,'ace_crc::crc16ccitt_nibble::crc_update()'],['../crc32__bit_8cpp.html#a63ddbf973341e053309ee7f31ea7ae4d',1,'ace_crc::crc32_bit::crc_update()'],['../crc32__byte_8cpp.html#ab36c06b3bef22d509a7ace196f30809c',1,'ace_crc::crc32_byte::crc_update()'],['../crc32__nibble_8cpp.html#a0c60d3ee23ae9b9bab6ef4bfa9434563',1,'ace_crc::crc32_nibble::crc_update()']]]
+];
