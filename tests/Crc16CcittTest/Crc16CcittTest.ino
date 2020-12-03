701e18a92af8875acbc0de23b5d63b1841990e61
Brian Park <brian@xparks.net>
2020-12-02 21:40:25 -0800

    tests: Verify that arrays of zero of different lengths resolve to different CRC values

--- tests/Crc16CcittTest/Crc16CcittTest.ino
+++ tests/Crc16CcittTest/Crc16CcittTest.ino
@@ -11,28 +11,96 @@ static const char CHECK_STRING[] = "123456789";
 static const size_t LENGTH = sizeof(CHECK_STRING) - 1;
 static uint16_t EXPECTED_CRC = 0xe5cc;
 
+static const size_t ZEROS_LENGTH = 4;
+static char ZEROES[ZEROS_LENGTH] = {0, 0, 0, 0};
+
 // --------------------------------------------------------------------------
-// crc16ccitt
+// crc16ccitt_bit
 // --------------------------------------------------------------------------
 
-test(crc16ccitt_bit, check) {
+uint16_t calcCrc16CcittBit(const char* data, size_t size) {
   uint16_t crc = crc16ccitt_bit::crc_init();
-  crc = crc16ccitt_bit::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc16ccitt_bit::crc_finalize(crc);
+  crc = crc16ccitt_bit::crc_update(crc, data, size);
+  return crc16ccitt_bit::crc_finalize(crc);
+}
+
+test(crc16ccitt_bit, check) {
+  uint16_t crc = calcCrc16CcittBit(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
+}
+
+// Verify that arrays of zeros of different length have different CRCs
+test(crc16ccitt_bit, zeros) {
+  uint16_t crc1 = calcCrc16CcittBit(ZEROES, 1);
+  uint16_t crc2 = calcCrc16CcittBit(ZEROES, 2);
+  uint16_t crc3 = calcCrc16CcittBit(ZEROES, 3);
+  uint16_t crc4 = calcCrc16CcittBit(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
+}
+
+// --------------------------------------------------------------------------
+// crc16ccitt_nibble
+// --------------------------------------------------------------------------
+
+uint16_t calcCrc16CcittNibble(const char* data, size_t size) {
+  uint16_t crc = crc16ccitt_nibble::crc_init();
+  crc = crc16ccitt_nibble::crc_update(crc, data, size);
+  return crc16ccitt_nibble::crc_finalize(crc);
+}
+
+// Verify that arrays of zeros of different length have different CRCs
+test(crc16ccitt_nibble, zeros) {
+  uint16_t crc1 = calcCrc16CcittNibble(ZEROES, 1);
+  uint16_t crc2 = calcCrc16CcittNibble(ZEROES, 2);
+  uint16_t crc3 = calcCrc16CcittNibble(ZEROES, 3);
+  uint16_t crc4 = calcCrc16CcittNibble(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
 }
 
 test(crc16ccitt_nibble, check) {
-  uint16_t crc = crc16ccitt_nibble::crc_init();
-  crc = crc16ccitt_nibble::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc16ccitt_nibble::crc_finalize(crc);
+  uint16_t crc = calcCrc16CcittNibble(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
 }
 
-test(crc16ccitt_byte, check) {
+// --------------------------------------------------------------------------
+// crc16ccitt_byte
+// --------------------------------------------------------------------------
+
+uint16_t calcCrc16CcittByte(const char* data, size_t size) {
   uint16_t crc = crc16ccitt_byte::crc_init();
-  crc = crc16ccitt_byte::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc16ccitt_byte::crc_finalize(crc);
+  crc = crc16ccitt_byte::crc_update(crc, data, size);
+  return crc16ccitt_byte::crc_finalize(crc);
+}
+
+// Verify that arrays of zeros of different length have different CRCs
+test(crc16ccitt_byte, zeros) {
+  uint16_t crc1 = calcCrc16CcittByte(ZEROES, 1);
+  uint16_t crc2 = calcCrc16CcittByte(ZEROES, 2);
+  uint16_t crc3 = calcCrc16CcittByte(ZEROES, 3);
+  uint16_t crc4 = calcCrc16CcittByte(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
+}
+
+test(crc16ccitt_byte, check) {
+  uint16_t crc = calcCrc16CcittBit(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
 }
 
--- tests/Crc32Test/Crc32Test.ino
+++ tests/Crc32Test/Crc32Test.ino
@@ -11,28 +11,96 @@ static const char CHECK_STRING[] = "123456789";
 static const size_t LENGTH = sizeof(CHECK_STRING) - 1;
 static uint32_t EXPECTED_CRC = 0xcbf43926;
 
+static const size_t ZEROS_LENGTH = 4;
+static char ZEROES[ZEROS_LENGTH] = {0, 0, 0, 0};
+
 // --------------------------------------------------------------------------
-// crc32
+// crc32_bit
 // --------------------------------------------------------------------------
 
-test(crc32_bit, check) {
+uint32_t calcCrc32Bit(const char* data, size_t size) {
   uint32_t crc = crc32_bit::crc_init();
-  crc = crc32_bit::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc32_bit::crc_finalize(crc);
+  crc = crc32_bit::crc_update(crc, data, size);
+  return crc32_bit::crc_finalize(crc);
+}
+
+test(crc32_bit, check) {
+  uint32_t crc = calcCrc32Bit(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
+}
+
+// Verify that arrays of zeros of different length have different CRCs
+test(crc32_bit, zeros) {
+  uint32_t crc1 = calcCrc32Bit(ZEROES, 1);
+  uint32_t crc2 = calcCrc32Bit(ZEROES, 2);
+  uint32_t crc3 = calcCrc32Bit(ZEROES, 3);
+  uint32_t crc4 = calcCrc32Bit(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
+}
+
+// --------------------------------------------------------------------------
+// crc32_nibble
+// --------------------------------------------------------------------------
+
+uint32_t calcCrc32Nibble(const char* data, size_t size) {
+  uint32_t crc = crc32_nibble::crc_init();
+  crc = crc32_nibble::crc_update(crc, data, size);
+  return crc32_nibble::crc_finalize(crc);
 }
 
 test(crc32_nibble, check) {
-  uint32_t crc = crc32_nibble::crc_init();
-  crc = crc32_nibble::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc32_nibble::crc_finalize(crc);
+  uint32_t crc = calcCrc32Nibble(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
 }
 
-test(crc32_byte, check) {
+// Verify that arrays of zeros of different length have different CRCs
+test(crc32_nibble, zeros) {
+  uint32_t crc1 = calcCrc32Nibble(ZEROES, 1);
+  uint32_t crc2 = calcCrc32Nibble(ZEROES, 2);
+  uint32_t crc3 = calcCrc32Nibble(ZEROES, 3);
+  uint32_t crc4 = calcCrc32Nibble(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
+}
+
+// --------------------------------------------------------------------------
+// crc32_byte
+// --------------------------------------------------------------------------
+
+uint32_t calcCrc32Byte(const char* data, size_t size) {
   uint32_t crc = crc32_byte::crc_init();
-  crc = crc32_byte::crc_update(crc, CHECK_STRING, LENGTH);
-  crc = crc32_byte::crc_finalize(crc);
+  crc = crc32_byte::crc_update(crc, data, size);
+  return crc32_byte::crc_finalize(crc);
+}
+
+// Verify that arrays of zeros of different length have different CRCs
+test(crc32_byte, zeros) {
+  uint32_t crc1 = calcCrc32Byte(ZEROES, 1);
+  uint32_t crc2 = calcCrc32Byte(ZEROES, 2);
+  uint32_t crc3 = calcCrc32Byte(ZEROES, 3);
+  uint32_t crc4 = calcCrc32Byte(ZEROES, 4);
+
+  assertNotEqual(crc1, crc2);
+  assertNotEqual(crc1, crc3);
+  assertNotEqual(crc1, crc4);
+  assertNotEqual(crc2, crc3);
+  assertNotEqual(crc2, crc4);
+  assertNotEqual(crc3, crc4);
+}
+
+test(crc32_byte, check) {
+  uint32_t crc = calcCrc32Byte(CHECK_STRING, LENGTH);
   assertEqual(EXPECTED_CRC, crc);
 }
 
