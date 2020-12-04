f994c5d835db5c91c51ec1f7e52d0fc0fb7d49e4
Brian Park <brian@xparks.net>
2020-12-03 22:37:26 -0800

    docs: Regenerate doxygen docs for v0.3

--- docs/html/search/files_0.js
+++ docs/html/search/files_0.js
@@ -1,15 +1,21 @@
 var searchData=
 [
-  ['crc16ccitt_5fbit_2ecpp_21',['crc16ccitt_bit.cpp',['../crc16ccitt__bit_8cpp.html',1,'']]],
-  ['crc16ccitt_5fbit_2ehpp_22',['crc16ccitt_bit.hpp',['../crc16ccitt__bit_8hpp.html',1,'']]],
-  ['crc16ccitt_5fbyte_2ecpp_23',['crc16ccitt_byte.cpp',['../crc16ccitt__byte_8cpp.html',1,'']]],
-  ['crc16ccitt_5fbyte_2ehpp_24',['crc16ccitt_byte.hpp',['../crc16ccitt__byte_8hpp.html',1,'']]],
-  ['crc16ccitt_5fnibble_2ecpp_25',['crc16ccitt_nibble.cpp',['../crc16ccitt__nibble_8cpp.html',1,'']]],
-  ['crc16ccitt_5fnibble_2ehpp_26',['crc16ccitt_nibble.hpp',['../crc16ccitt__nibble_8hpp.html',1,'']]],
-  ['crc32_5fbit_2ecpp_27',['crc32_bit.cpp',['../crc32__bit_8cpp.html',1,'']]],
-  ['crc32_5fbit_2ehpp_28',['crc32_bit.hpp',['../crc32__bit_8hpp.html',1,'']]],
-  ['crc32_5fbyte_2ecpp_29',['crc32_byte.cpp',['../crc32__byte_8cpp.html',1,'']]],
-  ['crc32_5fbyte_2ehpp_30',['crc32_byte.hpp',['../crc32__byte_8hpp.html',1,'']]],
-  ['crc32_5fnibble_2ecpp_31',['crc32_nibble.cpp',['../crc32__nibble_8cpp.html',1,'']]],
-  ['crc32_5fnibble_2ehpp_32',['crc32_nibble.hpp',['../crc32__nibble_8hpp.html',1,'']]]
+  ['crc16ccitt_5fbit_2ecpp_27',['crc16ccitt_bit.cpp',['../crc16ccitt__bit_8cpp.html',1,'']]],
+  ['crc16ccitt_5fbit_2ehpp_28',['crc16ccitt_bit.hpp',['../crc16ccitt__bit_8hpp.html',1,'']]],
+  ['crc16ccitt_5fbyte_2ecpp_29',['crc16ccitt_byte.cpp',['../crc16ccitt__byte_8cpp.html',1,'']]],
+  ['crc16ccitt_5fbyte_2ehpp_30',['crc16ccitt_byte.hpp',['../crc16ccitt__byte_8hpp.html',1,'']]],
+  ['crc16ccitt_5fnibble_2ecpp_31',['crc16ccitt_nibble.cpp',['../crc16ccitt__nibble_8cpp.html',1,'']]],
+  ['crc16ccitt_5fnibble_2ehpp_32',['crc16ccitt_nibble.hpp',['../crc16ccitt__nibble_8hpp.html',1,'']]],
+  ['crc32_5fbit_2ecpp_33',['crc32_bit.cpp',['../crc32__bit_8cpp.html',1,'']]],
+  ['crc32_5fbit_2ehpp_34',['crc32_bit.hpp',['../crc32__bit_8hpp.html',1,'']]],
+  ['crc32_5fbyte_2ecpp_35',['crc32_byte.cpp',['../crc32__byte_8cpp.html',1,'']]],
+  ['crc32_5fbyte_2ehpp_36',['crc32_byte.hpp',['../crc32__byte_8hpp.html',1,'']]],
+  ['crc32_5fnibble_2ecpp_37',['crc32_nibble.cpp',['../crc32__nibble_8cpp.html',1,'']]],
+  ['crc32_5fnibble_2ehpp_38',['crc32_nibble.hpp',['../crc32__nibble_8hpp.html',1,'']]],
+  ['crc8_5fbit_2ecpp_39',['crc8_bit.cpp',['../crc8__bit_8cpp.html',1,'']]],
+  ['crc8_5fbit_2ehpp_40',['crc8_bit.hpp',['../crc8__bit_8hpp.html',1,'']]],
+  ['crc8_5fbyte_2ecpp_41',['crc8_byte.cpp',['../crc8__byte_8cpp.html',1,'']]],
+  ['crc8_5fbyte_2ehpp_42',['crc8_byte.hpp',['../crc8__byte_8hpp.html',1,'']]],
+  ['crc8_5fnibble_2ecpp_43',['crc8_nibble.cpp',['../crc8__nibble_8cpp.html',1,'']]],
+  ['crc8_5fnibble_2ehpp_44',['crc8_nibble.hpp',['../crc8__nibble_8hpp.html',1,'']]]
 ];
